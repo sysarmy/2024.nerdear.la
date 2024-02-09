370b0ea59f58087ecd943a2e3d038159f4175165
Ariel Jolo <ajolo@sysarmy.com>
2024-02-09 01:43:29 -0300

    update

--- app/build/static/js/toggleVisibility.js
+++
@@ -1,6 +0,0 @@
-function toggleVisibility(...querySelectors) {
-	querySelectors.forEach((querySelector) => {
-		let element = document.querySelector(querySelector);
-		element.classList.toggle("d-none");
-	});
-}
